8d448314522265b0e49e72c8108779f9098cd0f0
tsironisper <alkis_1998@hotmail.com>
2024-06-01 20:38:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,11 +15,11 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {
+                        {
                                             // Budget component
-                            <div className='col-sm'>
-                                <Budget />
-                            </div>
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
                         }        
 
                         {
