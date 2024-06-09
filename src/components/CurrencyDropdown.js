213013cd283c3acef82fbc39c094dbe7488469e2
tsironisper <alkis_1998@hotmail.com>
2024-06-09 18:53:32 +0300

    Update CurrencyDropdown.js

--- src/components/CurrencyDropdown.js
+++ src/components/CurrencyDropdown.js
@@ -1,29 +1,23 @@
-// CurrencyDropdown.js
 
-import React, { useState } from "react";
-import { useCurrency } from "./CurrencyProvider";
-
+import React, { useState } from 'react';
+ 
 const CurrencyDropdown = () => {
-  const { selectedCurrency, changeCurrency } = useCurrency();
+const [selectedCurrency, setSelectedCurrency] = useState('$'); // Default currency
 
-  const handleCurrencyChange = (event) => {
-    changeCurrency(event.target.value);
-  };
-  return (
-    <div className="alert alert-primary">
-      <label htmlFor="currency">Currency:</label>
-      <select
-        id="currency"
-        value={selectedCurrency}
-        onChange={handleCurrencyChange}
-      >
-        <option value="$">$ Dollar</option>
-        <option value="£">£ Pound</option>
-        <option value="€">€ Euro</option>
-        <option value="₹">₹ Rupee</option>
-        {/* Add more currency options as needed */}
-      </select>
-    </div>
-  );
+const handleCurrencyChange = (event) => {
+setSelectedCurrency(event.target.value);
+};
+return (
+<div className='alert alert-primary'>
+<label htmlFor="currency">Currency:</label>
+<select id="currency" value={selectedCurrency} onChange={handleCurrencyChange}>
+<option value="$">$ Dollar</option>
+<option value="£">£ Pound</option>
+<option value="€">€ Euro</option>
+<option value="₹">₹ Rupee</option> 
+{/* Add more currency options as needed */}
+</select>
+</div>
+);
 };
 export default CurrencyDropdown;
