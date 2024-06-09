56e859f9ca8803fa66a2f86894e61caa77bbe1b4
tsironisper <alkis_1998@hotmail.com>
2024-06-09 18:55:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
-
+import { useCurrency } from "./CurrencyProvider";
 const App = () => {
     return (
         <AppProvider>
