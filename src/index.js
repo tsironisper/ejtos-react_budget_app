a2c5e21a01551a2b33f7041e79914a57ecec1b8d
tsironisper <alkis_1998@hotmail.com>
2024-06-09 19:00:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ root.render(
   </React.StrictMode>
 );
 
+//index.js
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CurrencyProvider>
+      <App />
+    </CurrencyProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
